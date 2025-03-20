4ee15ec14cd47a7c782eaff5cfd9ad60c422caf9
void <jijigh1124@naver.com>
2025-03-20 11:56:23 +0900

    v.1

---
+++ week03_prime.py
@@ -0,0 +1,16 @@
+number = int(input())
+is_prime = True
+
+if number >= 2:
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            print(i, end=" ")
+else:
+    is_prime = False
+
+
+    if is_prime:
+        print(f"{number}은 소수입니다")
+    else:
+        print(f"{number}은 소수가 아닙니다!")
